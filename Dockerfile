5c5089b2fb050eb520dd917274c287718b391720
Jonathan Izquierdo Galán <jizquierdo@dapda.com>
2019-02-01 09:45:12 +0100

    [UPD] First version poc

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM nginx:stable-alpine
-COPY index.php /code/
-COPY site.conf /etc/nginx/conf.d/default.conf
--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Hello world!</h1>
-<p>Hello there from IP <?php echo $_SERVER['SERVER_ADDR']; ?> </p>
-
-</body>
-</html>
